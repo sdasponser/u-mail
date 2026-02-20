7f65a88e181542afeedb991daf58db6acd63cc85
sdasponser <sdasponser@gmail.com>
2026-02-20 15:08:18 -0500

    Asset repletion

--- assets/checkPwd.js
+++ assets/checkPwd.js
@@ -3,7 +3,7 @@ document.getElementById("login").addEventListener("submit", function(e)
     e.preventDefault();
     const pwd = document.getElementById("pwd").value.toUpperCase();
 
-    if(pwd==="W3ST1S8E57!")
+    if(pwd==="W3ST1S8E57")
     {
         window.location.href = "./4e4f204348454154494e47.html";
     }
